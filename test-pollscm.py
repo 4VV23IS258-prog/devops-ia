054973105300484eb08ab07a292d031426ba61c1
Sumukha R Bayari <4VV23IS258@vvce.ac.in>
2026-03-15 22:54:58 +0530

    Add test script for build verification

---
+++ test-pollscm.py
@@ -0,0 +1,7 @@
+content=[
+  "REMOVE THIS CONTENT IN THE NEXT COMMIT AND CHECK IF THE BUILD IS GENERATED"
+]
+if len(content)==1:
+  print("Build Complete Now Change the File in Repository and see if build is generate again")
+else:
+  print("Build Rebuild Complete,After hearing changes in REPOSITORY")
